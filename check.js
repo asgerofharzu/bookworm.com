8e0b3aee01d0a32c6bb1b63fe0a2dc052b9bcfce
Emil Abdulxalıqov <emilabdulxaliqov@Emils-MacBook-Air.local>
2021-07-25 22:08:00 +0400

    location

--- check.js
+++ check.js
@@ -1,14 +1,14 @@
 $(function(){
     var keyChecker=window.localStorage.getItem('keyWord');
     if (!keyChecker) {
-        window.location.href='../singup.html';
+        window.location.href='singup.html';
         return;
     }
     var db=firebase.database();
     db.ref(`users/${keyChecker}`).once('value',function(snapshot){
         var info=snapshot.val();
         if (!info) {
-            window.location.href='../singup.html';
+            window.location.href='singup.html';
             return;
         }
         $("#firebase-name>span").html(info.userName);
